1a878b40bb54153a2be5db56e55d7dcbf0b5190c
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-01 12:57:10 -0300

    cpf validation

---
+++ index.js
@@ -0,0 +1,50 @@
+class CPFValidation {
+  constructor(cpf) {
+    Object.defineProperty(this, 'normalizedCpf', {
+      writable: false,
+      enumerable: false,
+      configurable: false,
+      value: cpf.replace(/\D+/g, ''),
+    })
+  }
+  static generateDigit(normalizedCpf) {
+    let total = 0
+    let reverse = normalizedCpf.length + 1
+
+    for (let cpfStringNumber of normalizedCpf) {
+      total += reverse * Number(cpfStringNumber)
+      reverse--
+    }
+
+    const digit = 11 - (total % 11)
+
+    return digit <= 9 ? String(digit) : '0'
+  }
+  generateCpf() {
+    const normalizedCpf = this.normalizedCpf.slice(0, -2)
+    const d1 = CPFValidation.generateDigit(normalizedCpf)
+    const d2 = CPFValidation.generateDigit(normalizedCpf + d1)
+    return (this.newCpf = normalizedCpf + d1 + d2)
+  }
+  isSequence() {
+    return this.normalizedCpf.charAt(0).repeat(11) === this.normalizedCpf
+  }
+  validation() {
+    if (!this.normalizedCpf) return false
+    if (typeof this.normalizedCpf !== 'string') return false
+    if (this.normalizedCpf.length !== 11) return false
+    if (this.isSequence()) return false
+    this.generateCpf()
+    console.log('newCpf', this.newCpf)
+
+    return this.newCpf === this.normalizedCpf
+  }
+}
+
+const CpfValidation = new CPFValidation('070.987.720-03')
+
+if (CpfValidation.validation()) {
+  console.log('CPF válido')
+} else {
+  console.log('CPF inválido')
+}
